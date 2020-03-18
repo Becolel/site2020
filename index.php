b06a1e35cc90f94d9f6f4102dc364ad6ffb6c421
Beco2020 <contato.becolel@gmail.com>
2020-03-18 08:44:51 -0300

    20202003

---
+++ index.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>beco</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/css.css">
+  </head>
+  <body class="text-black">
+    <header>
+      <nav class="navbar navbar-expand-md navbar-light fixed-top">
+        <a class="navbar-brand" href="index.php">
+          <img src="img/beco_logo.png" width="30" height="30" class="d-inline-block align-top">
+          BECO
+        </a>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="sobrenos.php">Sobre Nos</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="encontros.php">Encontros </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="faleConosco.php">Fale Conosco</a>
+            </li>
+          </ul>
+        </div>
+        <a class="navbar-brand" href="https://www.facebook.com/becolel/">
+          <img src="img/facebook.png" width="30" height="30" class="d-inline-block align-center">
+        </a>
+        <a class="navbar-brand" href="https://www.instagram.com/becolel/">
+          <img src="img/instagram.png" width="30" height="30" class="d-inline-block align-center">
+        </a>
+        <a class="navbar-brand" href="https://arq.ifsp.edu.br/portal/">
+          <img src="img/logo_ifsp.png" width="150" height="40" class="d-inline-block align-center">
+        </a>
+      </nav>
+      </div>
+    </header>
+    <!--Titulo-->
+      <div class="titulo">
+        <img src="img/logo_grande.png" class="card-img">
+      </div>
+    <!--Carrossel-->
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+    <div class="carousel-inner" >
+      <div class="carousel-item active">
+        <img src="img/beco9.jpg" class="img-fluid" alt="Imagem responsiva">
+          <div class="carousel-caption text-right">
+            <h3>Fique por dentro do que rolou </br>em nosso ultimo encontro!</h3>
+            <p>Agro é tech, Agro é pop, Agro é tudo? 24 de outubro de 2019.</p>
+            <p><a class="btn btn-lg btn-primary" href="#" >Veja mais</a></p>
+          </div>
+      </div>
+      <div class="carousel-item">
+      <img src="img/beco12.jpg" class="img-fluid" alt="Imagem responsiva">
+       <div class="carousel-caption text-right">
+            <h3>Fique por dentro do que rolou </br>em nosso ultimo encontro!</h3>
+            <p>Ciência ora quê? Ciência pra quem? 17 de setembro de 2019.</p>
+            <p><a class="btn btn-lg btn-primary" href="#" >Veja mais</a></p>
+          </div>
+      </div>
+      <div class="carousel-item">
+      <img src="img/beco13.jpg" class="img-fluid" alt="Imagem responsiva">
+      <div class="carousel-caption text-right">
+            <h3>Fique por dentro do que rolou </br>em nosso ultimo encontro!</h3>
+            <p>Ser ou não ser? 28 de Maio de 2019.</p>
+            <p><a class="btn btn-lg btn-primary" href="#" >Veja mais</a></p>
+          </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon"></span>
+    </a><!--botão anterior que muda o carrosel-->
+    <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+      <span class="carousel-control-next-icon"></span><!--botão proximo que muda o carrosel-->
+    </a>
+  </div>
+  <!-- FOOTER -->
+  <footer class="footer text-center">
+  Rua Doutor Aldo Benedito Pierri, 250
+  </br>Jardim dos Manacás
+  </br>Araraquara - SP
+  </br>14.804-296
+  </br>©2020 by Beco Literatura e Linguagem
+  </footer>
+    <script src="js/jquery-3.3.1.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/validaform.min.js"></script>
+</body>
+</html>
---
+++ sobrenos.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>beco</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/css.css">
+  </head>
+  <body class="text-black">
+    <header>
+      <nav class="navbar navbar-expand-md navbar-light fixed-top">
+        <a class="navbar-brand" href="index.php">
+          <img src="img/beco_logo.png" width="30" height="30" class="d-inline-block align-top">
+          BECO
+        </a>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="sobrenos.php">Sobre Nos</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="encontros.php">Encontros </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="faleConosco.php">Fale Conosco</a>
+            </li>
+          </ul>
+        </div>
+        <a class="navbar-brand" href="https://www.facebook.com/becolel/">
+          <img src="img/facebook.png" width="30" height="30" class="d-inline-block align-center">
+        </a>
+        <a class="navbar-brand" href="https://www.instagram.com/becolel/">
+          <img src="img/instagram.png" width="30" height="30" class="d-inline-block align-center">
+        </a>
+        <a class="navbar-brand" href="https://arq.ifsp.edu.br/portal/">
+          <img src="img/logo_ifsp.png" width="150" height="40" class="d-inline-block align-center">
+        </a>
+      </nav>
+      </div>
+    </header>
+    <!--Titulo-->
+      <div class="card ">
+        
+      </div>
+    
+  <!-- FOOTER -->
+  <footer class="footer   text-center">
+    by @Becolel.
+    <p>&copy; 2017-2019 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+    </div>
+  </footer>
+    <script src="js/jquery-3.3.1.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/validaform.min.js"></script>
+</body>
+</html>
