0a22c0df865ecdc4f9585636592a6a9661b1e771
Becolel <contato.becolel@gmail.com>
2020-03-26 03:55:40 -0300

    20200326

--- index.php
+++ index.php
@@ -8,41 +8,13 @@
     <link rel="stylesheet" href="css/css.css">
   </head>
   <body >
-    <header>
-      <nav class="navbar navbar-expand-md navbar-light fixed-top" style="background-color:white;">
-        <a class="navbar-brand" href="index.php">
-          <img src="img/logos/beco_logo.png" width="30" height="30" class="d-inline-block align-top">
-          BECO
-        </a>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="sobrenos.php">Sobre Nos</a>
-            </li>
-            <li class="nav-item active">
-              <a class="nav-link" href="encontros.php">Encontros </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="faleConosco.php">Fale Conosco</a>
-            </li>
-          </ul>
-        </div>
-        <a class="navbar-brand" href="https://www.facebook.com/becolel/">
-          <img src="img/logos/facebook.png" width="30" height="30" class="d-inline-block align-center">
-        </a>
-        <a class="navbar-brand" href="https://www.instagram.com/becolel/">
-          <img src="img/logos/instagram.png" width="30" height="30" class="d-inline-block align-center">
-        </a>
-        <a class="navbar-brand" href="https://arq.ifsp.edu.br/portal/">
-          <img src="img/logos/logo_ifsp.png" width="150" height="40" class="d-inline-block align-center">
-        </a>
-      </nav>
-      </div>
-    </header>
+    <?php
+      include "cabecalho.inc";
+    ?>
     <!--Titulo-->
-      <div class="titulo">
-        <img src="img/logos/logo_grande2.png" style=" margin-left: 35%;" >
-      </div> 
+    <div class="titulo">
+      <img src="img/logos/logo_grande2.png" class="img-fluid mx-auto d-block"  >
+    </div> 
     <!--Carrossel-->
     <div id="myCarousel" class="carousel slide" data-ride="carousel">
       <ol class="carousel-indicators">
@@ -50,9 +22,9 @@
         <li data-target="#myCarousel" data-slide-to="1"style="background-color:rgb(247, 41, 41);"></li>
         <li data-target="#myCarousel" data-slide-to="2"style="background-color:rgb(247, 41, 41);"></li>
       </ol>
-    <div class="carousel-inner" >
-      <div class="carousel-item active">
-        <img src="img/encontros/beco_agro.jpg" class="img-fluid" >
+      <div class="carousel-inner" >
+        <div class="carousel-item active">
+          <img src="img/encontros/beco_agro.jpg" class="img-fluid" >
           <div class="carousel-caption text-right">
             <h3 style="color: #53BC28;">Fique por dentro do que rolou </br>em nosso ultimo encontro!</h3>
             <p>Agro é tech, Agro é pop, Agro é tudo? 24 de outubro de 2019.</p>
@@ -60,20 +32,20 @@
           </div>
       </div>
       <div class="carousel-item">
-      <img src="img/encontros/beco-ciencia.jpg" class="img-fluid" alt="Imagem responsiva">
-       <div class="carousel-caption text-right">
+        <img src="img/encontros/beco-ciencia.jpg" class="img-fluid" alt="Imagem responsiva">
+        <div class="carousel-caption text-right">
             <h3 style="color: #53BC28;">Fique por dentro do que rolou </br>no encontro de Setembro!</h3>
             <p>Ciência ora quê? Ciência pra quem? 17 de setembro de 2019.</p>
             <p><a class="btn btn-lg btn-success" style="background-color: #53BC28;" href="#" >Veja mais</a></p>
-          </div>
+        </div>
       </div>
       <div class="carousel-item">
-      <img src="img/encontros/beco_desconectar.png" class="img-fluid" alt="Imagem responsiva">
-      <div class="carousel-caption text-right">
-            <h3 style="color: #53BC28;">Fique por dentro do que rolou </br>no encontro de Julho!</h3>
-            <p>Desconectar para Conectar!10 de junho de 2019</p>
-            <p><a class="btn btn-lg btn-success" style="background-color: #53BC28;" href="#" >Veja mais</a></p>
-          </div>
+        <img src="img/encontros/beco_desconectar.png" class="img-fluid" alt="Imagem responsiva">
+        <div class="carousel-caption text-right">
+              <h3 style="color: #53BC28;">Fique por dentro do que rolou </br>no encontro de Julho!</h3>
+              <p>Desconectar para Conectar!10 de junho de 2019</p>
+              <p><a class="btn btn-lg btn-success" style="background-color: #53BC28;" href="#" >Veja mais</a></p>
+        </div>
       </div>
     </div>
     <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
@@ -89,9 +61,9 @@
   </div>
 </div>
     <!-- COMEÇO QUADRADOS -->
-  <div class="text-white">
-    <div class="row">
-      <div class="col" style="background-color:rgb(247, 41, 41); text-align:center; padding:10% 3% 5% 3%;">
+  <div class="container text-white">
+    <div class="row align-items-center" >
+      <div class="col-12 col-md-6" style="background-color:rgb(247, 41, 41); text-align:center; padding:10% 3% 5% 3%;">
       <h4>Sejam Bem Vindos!</h4>
         <h2 >Vem aí... Beco 2020</h2>
         <p class="lead">
@@ -100,15 +72,15 @@
             fique por dentro dos eventos e participe dos encontros mensais! Não perca esta oportunidade! 
         </p>
       </div>
-      <div class="col">
-      <img src="img/equipe-finalEncontro2.jpg" class="img-fluid" width="100%" height="90%">
+      <div class="col-12 col-md-6">
+      <img src="img/equipe-finalEncontro2.jpg"  class="img-fluid"> <!-- width="100%" height="90%"--> 
       </div>
     </div>
-    <div class="row">
-      <div class="col" >
-        <img src="img/equipe2019.jpg" class="img-fluid" width="100%" height="90%">
+    <div class="row align-items-center">
+      <div class="col-12 col-md-6" >
+        <img src="img/equipe2019.jpg"  class="img-fluid" >
       </div>
-      <div class="col" style="background-color:black; text-align:center; padding:15% 3% 5% 3%;">
+      <div class="col-12 col-md-6" style="background-color:black; text-align:center; padding:15% 3% 5% 3%;">
         <h2> Beco e sua História</h2>
         <p class="lead">
           Ficou curioso? Quer saber mais sobre o projeto? Clique no botão 
@@ -118,8 +90,8 @@
         <p><a class="btn btn-outline-success"  href="#" >Veja mais</a></p>
       </div>
     </div>
-    <div class="row ">
-      <div class="col" style="background-color: #53BC28; text-align:center; padding:8% 3% 0% 3%;">
+    <div class="row align-items-center" >
+      <div class="col-12 col-md-6" style="background-color: #53BC28; text-align:center; padding:8% 3% 0% 3%;">
         <h2 >IFSP ARARAQUARA</h2>
         <p class="lead">Venha conhecer o IFSP </br> Acesse o site do Campus Araraquara 
         e fique por dentro de tudo o que acontece aqui. Entre em contato e agende sua 
@@ -127,20 +99,15 @@
         </p>
         <p><a class="btn btn-outline-danger" href="#" >Veja mais</a></p>
       </div>
-      <div class="col">
-      <img src="img/ifsp.jpg" class="img-fluid" width="100%" height="90%">
+      <div class="col-12 col-md-6" >
+      <img src="img/ifsp.jpg" class="img-fluid" >
       </div>
     </div>
   </div>
   <!-- /  FIM QUADRADOS-->
-  <!-- FOOTER -->
-  <footer class="footer text-center">
-  Rua Doutor Aldo Benedito Pierri, 250
-  </br>Jardim dos Manacás
-  </br>Araraquara - SP
-  </br>14.804-296
-  </br>©2020 by Beco Literatura e Linguagem
-  </footer>
+    <?php
+      include "rodape.inc";
+    ?>
     <script src="js/jquery-3.3.1.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
 		<script src="js/validaform.min.js"></script>
